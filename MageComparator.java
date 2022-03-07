43968802ab38b565d262457bc828517a408d0c48
Dar-Na <70232815+Dar-Na@users.noreply.github.com>
2022-03-07 19:40:24 +0100

    Create MageComparator.java

---
+++ MageComparator.java
@@ -0,0 +1,19 @@
+package pl.edu.pg.eti.ksg.po.lab1;
+
+import java.util.Comparator;
+
+public  class MageComparator implements Comparator<Mage> {
+
+    @Override
+    public int compare(Mage o1, Mage o2) {
+        int ret = Integer.compare(o1.getLevel(), o2.getLevel());
+        if ( ret != 0 ) {
+            return ret;
+        }
+        ret = o1.getName().compareTo( o2.getName() );
+        if ( ret != 0 ) {
+            return ret;
+        }
+        return Double.compare(o1.getPower(), o2.getPower());
+    }
+}
